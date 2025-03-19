8957fb34bc918181c71fc1ea13ed176923d88aef
tiitux1 <152963179+tiitux1@users.noreply.github.com>
2025-03-19 11:17:30 +0200

    3.2

--- puhelinluettelo-backend/index.js
+++ puhelinluettelo-backend/index.js
@@ -25,7 +25,7 @@ app.get('/info', (req, res) => {
     `);
 });
 
-// Käynnistetään palvelin
+// Käynnistetään palvelinta
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
